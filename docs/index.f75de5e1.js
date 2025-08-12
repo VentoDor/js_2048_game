bf8cef52537e6da25982508e7768c6452da6b39a
Dariia Hryshchukova <dariagrisukova@gmail.com>
2025-08-12 03:03:53 +0300

    Publish static build to docs for GH Pages

---
+++ docs/index.f75de5e1.js
@@ -0,0 +1,6 @@
+"use strict"; // Uncomment the next lines to use your game instance in the browser
+ // const Game = require('../modules/Game.class');
+ // const game = new Game();
+ // Write your code here
+
+//# sourceMappingURL=index.f75de5e1.js.map
