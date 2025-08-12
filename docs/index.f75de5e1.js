143de125cc577ec0dec2afb89e14df359f4361c7
Dariia Hryshchukova <dariagrisukova@gmail.com>
2025-08-12 16:26:32 +0300

    fix: rebuild docs with correct public-url

--- docs/index.f75de5e1.js
+++
@@ -1,6 +0,0 @@
-"use strict"; // Uncomment the next lines to use your game instance in the browser
- // const Game = require('../modules/Game.class');
- // const game = new Game();
- // Write your code here
-
-//# sourceMappingURL=index.f75de5e1.js.map
